def61b20a0273d84198f356254e12e2233995dd2
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2026-04-18 11:22:26 -0500

    refactor tweaks

--- src/controls.js
+++ src/controls.js
@@ -5,12 +5,18 @@ export const addControls = (controls) => {
   const { pointLight, ambientLight, material } = controls;
 
   const lightFolder = gui.addFolder("Light");
+  const pointLightFolder = lightFolder.addFolder("Point Light");
   const materialFolder = gui.addFolder("Material");
 
-  lightFolder.add(pointLight.position, "x").min(-3).max(3).step(0.01);
-  lightFolder.add(pointLight.position, "y").min(-3).max(3).step(0.01);
-  lightFolder.add(pointLight.position, "z").min(-3).max(3).step(0.01);
-  lightFolder.add(ambientLight, "intensity").min(0).max(3).step(0.01);
+  pointLightFolder.add(pointLight.position, "x").min(-3).max(3).step(0.01);
+  pointLightFolder.add(pointLight.position, "y").min(-3).max(3).step(0.01);
+  pointLightFolder.add(pointLight.position, "z").min(-3).max(3).step(0.01);
+  lightFolder
+    .add(ambientLight, "intensity")
+    .min(0)
+    .max(3)
+    .step(0.01)
+    .name("ambientIntensity");
 
   materialFolder.add(material, "metalness").min(0).max(1).step(0.0001);
   materialFolder.add(material, "roughness").min(0).max(1).step(0.0001);
